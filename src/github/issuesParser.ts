9cb1a54b21c1920e0c79e355562d0142d6a73447
Javier Bullrich <javier.bullrich@parity.io>
2023-04-02 17:13:59 +0200

    added logic to handle pagination

--- src/github/issuesParser.ts
+++ src/github/issuesParser.ts
@@ -2,12 +2,29 @@ import { debug } from "@actions/core";
 import { GitHub } from "@actions/github/lib/utils";
 import moment from "moment";
 
+//** Handles the problem of pagination */
+const getAllIssues = async (octokit: InstanceType<typeof GitHub>, repo: Repo): Promise<IssueData[]> => {
+    const { data } = await octokit.rest.issues.listForRepo({ ...repo, per_page: 100, state: "open" });
+    let issues = data;
+    if (issues.length > 99) {
+        let fullPage = issues.length > 99;
+        let currentPage = 2;
+        while (fullPage) {
+            const { data } = await octokit.rest.issues.listForRepo({ ...repo, per_page: 100, page: currentPage, state: "open" });
+            issues = issues.concat(data);
+            fullPage = data.length > 99;
+            currentPage++;
+        }
+    }
+    return issues;
+}
+
 export const fetchIssues = async (octokit: InstanceType<typeof GitHub>, repo: Repo): Promise<IssueData[]> => {
-    const issues = await octokit.rest.issues.listForRepo({ ...repo, per_page: 100, state: "open" });
-    debug(`Found elements ${issues.data.length}`);
+    const issues = await getAllIssues(octokit, repo);
+    debug(`Found elements ${issues.length}`);
 
     // order them from stalest to most recent
-    const orderedDates = issues.data.sort((a, b) => {
+    const orderedDates = issues.sort((a, b) => {
         return b.updated_at > a.updated_at ? -1 : b.updated_at < a.updated_at ? 1 : 0
     });
 
