464eb59e786387013386cbbc6d8bc1f46a71f6d4
Javyer <javier.bullrich@parity.io>
2023-04-03 13:10:58 +0200

    Handle pagination (#8)
    Added the ability to iterate over pages.
    
    Resolves #4
    
    The logic is quite simple. It fetches items from a page. If there are
    more than 99 elements, then that page is full and it goes over to the
    next page.
    
    Upgraded version to `0.0.2` to have a new release

--- src/github/issuesParser.ts
+++ src/github/issuesParser.ts
@@ -2,12 +2,35 @@ import { debug } from "@actions/core";
 import { GitHub } from "@actions/github/lib/utils";
 import moment from "moment";
 
+const listForRepo = (octokit: InstanceType<typeof GitHub>, repo: Repo, per_page: number = 100, page: number = 1) => {
+    return octokit.rest.issues.listForRepo({ ...repo, per_page, state: "open", page });
+}
+
+//** Handles the problem of pagination */
+const getAllIssues = async (octokit: InstanceType<typeof GitHub>, repo: Repo): Promise<IssueData[]> => {
+    const perPage = 100;
+    let currentPage = 1;
+    const { data } = await listForRepo(octokit, repo, perPage, currentPage);
+    let issues = data;
+    let fullPage = issues.length > 99;
+    while (fullPage) {
+        currentPage++;
+        debug(`Iterating on page ${currentPage} with ${issues.length} issues`);
+        const { data } = await listForRepo(octokit, repo, perPage, currentPage)
+        issues = issues.concat(data);
+        fullPage = data.length > 99;
+    }
+
+    debug(`Found a total of ${issues.length} issues`);
+    return issues;
+}
+
 export const fetchIssues = async (octokit: InstanceType<typeof GitHub>, repo: Repo): Promise<IssueData[]> => {
-    const issues = await octokit.rest.issues.listForRepo({ ...repo, per_page: 100, state: "open" });
-    debug(`Found elements ${issues.data.length}`);
+    const issues = await getAllIssues(octokit, repo);
+    debug(`Found elements ${issues.length}`);
 
     // order them from stalest to most recent
-    const orderedDates = issues.data.sort((a, b) => {
+    const orderedDates = issues.sort((a, b) => {
         return b.updated_at > a.updated_at ? -1 : b.updated_at < a.updated_at ? 1 : 0
     });
 
