2690d230c2ee12245ce2ae20a7631e165ddb7036
Javyer <javier.bullrich@parity.io>
2023-05-03 14:32:47 +0200

    Implemented linting (#16)
    Implemented
    [`@eng-automation/js-style`](https://github.com/paritytech/opstooling-js-style)
    with the linting method.
    
    Created CI step for it to be tested.

--- src/index.ts
+++ src/index.ts
@@ -2,117 +2,122 @@ import { debug, getBooleanInput, getInput, info, setOutput, summary } from "@act
 import { context, getOctokit } from "@actions/github";
 import { Context } from "@actions/github/lib/context";
 import moment from "moment";
+
 import { byNoComments, isNotFromAuthor, olderThanDays } from "./filters";
 import { fetchIssues } from "./github/issuesParser";
 
-const daysSinceDate = (date: string): number => {
-    return moment().diff(moment(date), 'days')
-}
+const daysSinceDate = (date: string): number => moment().diff(moment(date), "days");
 
 const getFiltersFromInput = (): Filters => {
-    const inputDays = Number.parseInt(getInput("days-stale", { required: false }));
-    const daysStale = isNaN(inputDays) ? 5 : inputDays;
+  const inputDays = Number.parseInt(getInput("days-stale", { required: false }));
+  const daysStale = isNaN(inputDays) ? 5 : inputDays;
 
-    const noComments = !!getInput("noComments") ? getBooleanInput("noComments") : false;
+  const noComments = getInput("noComments") ? getBooleanInput("noComments") : false;
 
-    let ignoreAuthors: string[] = [];
-    const authorsToIgnore = getInput("ignoreAuthors");
-    if (authorsToIgnore) {
-        ignoreAuthors = authorsToIgnore.split(",");
-    }
+  let ignoreAuthors: string[] = [];
+  const authorsToIgnore = getInput("ignoreAuthors");
+  if (authorsToIgnore) {
+    ignoreAuthors = authorsToIgnore.split(",");
+  }
 
-    return {
-        daysStale, noComments, notFromAuthor: ignoreAuthors
-    }
-}
+  return { daysStale, noComments, notFromAuthor: ignoreAuthors };
+};
 
-const generateMarkdownMessage = (issues: IssueData[], repo: { owner: string, repo: string; }) => {
-    const messages = issues.map(issue => {
-        return `  - [${issue.title}](${issue.html_url}) - Stale for ${daysSinceDate(issue.updated_at)} days`;
-    });
-    const markdownMessage = `### Repo ${repo.owner}/${repo.repo} has ${issues.length} stale issues\n${messages.join("\n")}`;
-    return markdownMessage;
-}
+const generateMarkdownMessage = (issues: IssueData[], repo: { owner: string; repo: string }) => {
+  const messages = issues.map(
+    (issue) => `  - [${issue.title}](${issue.html_url}) - Stale for ${daysSinceDate(issue.updated_at)} days`,
+  );
+  return `### Repo ${repo.owner}/${repo.repo} has ${issues.length} stale issues\n${messages.join("\n")}`;
+};
 
-const getRepo = (ctx: Context): { owner: string, repo: string } => {
-    let repo = getInput("repo", { required: false });
-    if (!repo) {
-        repo = ctx.repo.repo;
-    }
+const getRepo = (ctx: Context): { owner: string; repo: string } => {
+  let repo = getInput("repo", { required: false });
+  if (!repo) {
+    repo = ctx.repo.repo;
+  }
 
-    let owner = getInput("owner", { required: false });
-    if (!owner) {
-        owner = ctx.repo.owner;
-    }
+  let owner = getInput("owner", { required: false });
+  if (!owner) {
+    owner = ctx.repo.owner;
+  }
 
-    return { repo, owner };
-}
+  return { repo, owner };
+};
 
-const filterIssues = (issues: IssueData[], filters: Filters) => {
-    if (!issues || issues.length < 1) {
-        return [];
-    }
+const filterIssues = (issues: IssueData[] | undefined, filters: Filters) => {
+  if (!issues || issues.length < 1) {
+    return [];
+  }
 
-    let filteredData = issues;
-    if (filters.daysStale) {
-        filteredData = filteredData.filter(is => olderThanDays(is, filters.daysStale));
-    }
-    if (filters.noComments) {
-        filteredData = filteredData.filter(byNoComments);
-    }
-    if (filters.notFromAuthor && filters.notFromAuthor.length > 0) {
-        filteredData = filteredData.filter(is => isNotFromAuthor(is, filters.notFromAuthor));
-    }
+  let filteredData = issues;
+  if (filters.daysStale) {
+    filteredData = filteredData.filter((is) => olderThanDays(is, filters.daysStale));
+  }
+  if (filters.noComments) {
+    filteredData = filteredData.filter(byNoComments);
+  }
+  if (filters.notFromAuthor.length > 0) {
+    filteredData = filteredData.filter((is) => isNotFromAuthor(is, filters.notFromAuthor));
+  }
 
-    return filteredData;
-}
+  return filteredData;
+};
 
 const runAction = async (ctx: Context) => {
-    const repo = getRepo(ctx);
-    const token = getInput("GITHUB_TOKEN", { required: true });
+  const repo = getRepo(ctx);
+  const token = getInput("GITHUB_TOKEN", { required: true });
 
-    const filters = getFiltersFromInput();
-    debug(JSON.stringify(filters));
+  const filters = getFiltersFromInput();
+  debug(JSON.stringify(filters));
 
-    const octokit = getOctokit(token);
-    const staleIssues = await fetchIssues(octokit, repo);
+  const octokit = getOctokit(token);
+  const staleIssues = await fetchIssues(octokit, repo);
 
-    // we filter the issues and see how many are remaining
-    const filteredIssues = filterIssues(staleIssues, filters);
+  // we filter the issues and see how many are remaining
+  const filteredIssues = filterIssues(staleIssues, filters);
 
-    const amountOfStaleIssues = filteredIssues.length;
+  const amountOfStaleIssues = filteredIssues.length;
 
-    info(`Found ${amountOfStaleIssues} stale issues.`);
-    setOutput("repo", `${repo.owner}/${repo.repo}`);
-    setOutput("stale", amountOfStaleIssues);
+  info(`Found ${amountOfStaleIssues} stale issues.`);
+  setOutput("repo", `${repo.owner}/${repo.repo}`);
+  setOutput("stale", amountOfStaleIssues);
 
-    if (amountOfStaleIssues > 0) {
-        let cleanedData = filteredIssues.map(issue => {
-            return {
-                url: issue.html_url,
-                title: issue.title,
-                daysStale: daysSinceDate(issue.updated_at),
-                number: issue.number
-            }
-        });
+  if (amountOfStaleIssues > 0) {
+    const cleanedData = filteredIssues.map((issue) => {
+      return {
+        url: issue.html_url,
+        title: issue.title,
+        daysStale: daysSinceDate(issue.updated_at),
+        number: issue.number,
+      };
+    });
 
-        const jsonData = JSON.stringify(cleanedData)
-        setOutput("data", jsonData);
-        debug(jsonData);
-        const message = generateMarkdownMessage(filteredIssues, repo);
-        setOutput("message", message);
+    const jsonData = JSON.stringify(cleanedData);
+    setOutput("data", jsonData);
+    debug(jsonData);
+    const message = generateMarkdownMessage(filteredIssues, repo);
+    setOutput("message", message);
 
-        await summary.addHeading(`${repo.owner}/${repo.repo}`)
-            .addHeading(`${amountOfStaleIssues} stale issues`, 3)
-            .addTable([
-                [{ data: "Title", header: true }, { data: "Days stale", header: true }, { data: "Link", header: true }],
-                ...cleanedData.map(issue => [issue.title, issue.daysStale.toString(), `${repo.owner}/${repo.repo}#${issue.number}`] as string[])
-            ])
-            .addLink("See all issues", `https://github.com/${repo.owner}/${repo.repo}/issues`).write();
-    } else {
-        setOutput("message", `### Repo ${repo.owner}/${repo.repo} has no stale issues`);
-        info(`Repo ${repo.owner}/${repo.repo} has no stale issues`);
-    }
-}
+    await summary
+      .addHeading(`${repo.owner}/${repo.repo}`)
+      .addHeading(`${amountOfStaleIssues} stale issues`, 3)
+      .addTable([
+        [
+          { data: "Title", header: true },
+          { data: "Days stale", header: true },
+          { data: "Link", header: true },
+        ],
+        ...cleanedData.map(
+          (issue) =>
+            [issue.title, issue.daysStale.toString(), `${repo.owner}/${repo.repo}#${issue.number}`] as string[],
+        ),
+      ])
+      .addLink("See all issues", `https://github.com/${repo.owner}/${repo.repo}/issues`)
+      .write();
+  } else {
+    setOutput("message", `### Repo ${repo.owner}/${repo.repo} has no stale issues`);
+    info(`Repo ${repo.owner}/${repo.repo} has no stale issues`);
+  }
+};
 
 runAction(context);
